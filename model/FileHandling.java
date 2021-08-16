2cdb7f47940a03da75538583cedfae7ce95e171c
tararosenthal <65468485+tararosenthal@users.noreply.github.com>
2021-08-16 12:40:43 -0500

    Update FileHandling.java

--- model/FileHandling.java
+++ model/FileHandling.java
@@ -10,26 +10,6 @@ public class FileHandling {
     private final File receivedFile = new File("received.txt");
     private final File encodedFile = new File("encoded.txt");
     private final File decodedFile = new File("decoded.txt");
-    private File newFile; //allows creation of new file for data storage
-    /*
-     * Returns specific files based on string names. Returns a new file with given name if does correspond to set files.
-     * @param string            name of file requested
-     * @return File             file corresponding to requested
-     */
-    public File getFile(String string) {
-        string = string.toLowerCase().trim();
-        if ("send".equals(string)) {
-            return sendFile;
-        } else if ("received".equals(string)) {
-            return receivedFile;
-        } else if ("encoded".equals(string)) {
-            return encodedFile;
-        } else if ("decoded".equals(string)) {
-            return decodedFile;
-        } else {
-            return newFile = new File(string + ".txt");
-        }
-    }
 
     public File getSendFile() {
         return sendFile;
